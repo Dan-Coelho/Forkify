5634815ee026705bebae3286a6e8857d3746d952
Dan-Coelho <danpoco@gmail.com>
2023-10-30 15:39:50 -0300

    Eventos de hashchange and load

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -603,9 +603,11 @@ const renderSpinner = function(parentEl) {
 };
 const showRecipe = async function() {
     try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
         //1-loading recipe
         renderSpinner(recipeContainer);
-        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         let { recipe } = data.data;
@@ -713,7 +715,11 @@ const showRecipe = async function() {
         alert(err);
     }
 };
-showRecipe();
+//Eventos para renderizar receitas
+[
+    "hashchange",
+    "load"
+].forEach((ev)=>window.addEventListener(ev, showRecipe));
 
 },{"core-js/modules/web.immediate.js":"49tUX","url:../img/icons.svg":"loVOp","regenerator-runtime/runtime":"dXNgZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"49tUX":[function(require,module,exports) {
 "use strict";
